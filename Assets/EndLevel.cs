0b117652f130fdb1b11bdbafff9bf020073b5622
Bedlamikus <bedlamikus@gmail.com>
2023-07-14 01:29:57 +0700

    end level

--- Assets/EndLevel.cs
+++ Assets/EndLevel.cs
@@ -1,11 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using YG;
 
 public class EndLevel : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        print("LevelEnd");
+        if (other.GetComponent<Train>() == null) return;
+        var data = YandexGame.Instance.savesData();
+        data.levels[data.currentLevel].ended = true;
+        data.currentLevel++;
+        YandexGame.Instance._SaveProgress();
+        SceneManager.LoadScene(0);
     }
 }

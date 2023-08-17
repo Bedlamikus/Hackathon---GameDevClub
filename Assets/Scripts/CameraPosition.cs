a245cc71905c3a17dca4e9edf7862c4d97f38b14
bedlamikus@gmail.com <bedlamikus@gmail.com>
2023-08-17 16:49:54 +0700

    CameraPosition

--- Assets/Scripts/CameraPosition.cs
+++ Assets/Scripts/CameraPosition.cs
@@ -4,4 +4,27 @@ using UnityEngine;
 
 public class CameraPosition : MonoBehaviour
 {
+    [SerializeField] private bool target;
+    [SerializeField] private Train train;
+    [SerializeField] private float speed;
+    [SerializeField] private GameObject mayak;
+
+    private Vector3 trail;
+    private void Start()
+    {
+        if (target)
+        {
+            trail = train.transform.position;
+        }
+    }
+    private void Update()
+    {
+        if (!target) return;
+        transform.position = Vector3.Lerp(
+            transform.position, 
+            mayak.transform.position, 
+            speed * Time.deltaTime);
+        trail = Vector3.Lerp(trail, train.transform.position, speed * Time.deltaTime);
+        transform.LookAt(trail);
+    }
 }
